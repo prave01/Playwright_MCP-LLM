15c0ce5333a8f862098a5237870a29746e94dc07
PraveenThanikachalam <praveenthanikachalam408@gmail.com>
2025-05-25 21:09:34 +0530

    Connects client🔗server at 8080 ✅

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,41 @@
-import { Client } from "@modelcontextprotocol/sdk/client/index";
-import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp";
-import { SSEClientTransport } from "@modelcontextprotocol/sdk/client/sse";
-import { exec } from "child_process";
-type client_vars = {
-  client: { name: string; version: string };
-  purpose: string;
-  base_url: string;
-};
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { SSEClientTransport } from "@modelcontextprotocol/sdk/client/sse.js";
 
 class MCP_Client {
-  base_url: string;
-  client_fields: { name: string; version: string };
-  purpose: string;
-  // Initializes the client property with a new Client instance
-  constructor({ client, purpose, base_url }: client_vars) {
-    // Setting the variables for the MCP Client
-    this.base_url = base_url;
-    this.client_fields = client;
-    this.purpose = purpose;
+  port: string;
+
+  // Specify the port to connect with the server
+  constructor(server_port: string) {
+    this.port = server_port;
   }
 
-  // Starts the Playwright MCP Server
-  start_server(port: number, transport: string) {
-    exec(
-      `npx playwright-mcp-server --port ${port}`,
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Error starting server: ${error.message}`);
-          return;
-        }
-        if (stderr) {
-          console.error(`Server stderr: ${stderr}`);
-          return;
-        }
-        console.log(`Server started: ${stdout}`);
-      }
+  async Async_Client() {
+    // Transport layer for sending messages between server
+    // and client using Server-Sent Events
+
+    const transport = new SSEClientTransport(
+      new URL("http:localhost:8080/sse")
     );
-  }
 
-  connect_with_server() {}
+    const new_client = new Client({
+      name: "sse-client",
+      version: "1.0.0",
+    });
+
+    // Connect the server on port 8080 with client using SSE
+    await new_client
+      .connect(transport)
+      .then(() => {
+        console.log(`Conected with server using SSE transport`);
+      })
+      .catch((err) => {
+        console.error("Fkk ended up error :", err);
+      });
+  }
 }
+
+// New Client Instance
+
+const client = new MCP_Client("8080");
+
+client.Async_Client();
